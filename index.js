e83bbef46c4f2056088c56796f13597e02079999
Ricardo Guerra <RikkuG491@gmail.com>
2024-09-04 14:19:52 -0400

    Added Display Details Section to index.html, and created displayBurgerDetails() function in index.js and wrote some example code for it

--- index.js
+++ index.js
@@ -7,10 +7,21 @@ function addBurgerImageToRestaurantMenu(burger){
     restaurantMenuElement.appendChild(imgElement)
 }
 
+function displayBurgerDetails(burger){
+    console.log(burger)
+}
+
 fetch("http://localhost:3000/burgers")
 .then(response => response.json())
 .then(burgers => {
     burgers.forEach(addBurgerImageToRestaurantMenu)
+
+    if(burgers.length === 0){
+        alert("Warning: There are no burgers in the database!")
+    }
+    else{
+        displayBurgerDetails(burgers[0])
+    }
 })
 
 // At some point, we'll search for a <form> from the DOM (<form> doesn't exist yet, so we'll need to add it to the index.html file)
