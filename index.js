e88e9fb1607ac2816250a3a6acb7dbe102a34aae
Ricardo Guerra <RikkuG491@gmail.com>
2024-09-04 13:37:52 -0400

    Wrote code to iterate over burgers and called the addBurgerImageToRestaurantMenu() function for each iteration over the array stored in the burgers variable

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// This is a function that will add a burger image to the restaurant menu when the function is called.
+function addBurgerImageToRestaurantMenu(burger){
+    console.log(burger)
+}
+
 fetch("http://localhost:3000/burgers")
 .then(response => response.json())
 .then(burgers => {
-    console.log(burgers)
+    burgers.forEach(addBurgerImageToRestaurantMenu)
 })
+
+// At some point, we'll search for a <form> from the DOM (<form> doesn't exist yet, so we'll need to add it to the index.html file)
